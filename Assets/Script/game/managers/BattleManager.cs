202735fe0a2cb6c33a2129b5ca8e7ae75bdd61cc
Gabriel Piriz <lirianer@gmail.com>
2018-05-24 21:58:15 -0300

    Barebones of BattleManager

---
+++ Assets/Script/game/managers/BattleManager.cs
@@ -0,0 +1,37 @@
+class BattleManager
+{
+	// Equipo del Jugador
+	protected List<BattleEntity> playerParty;
+
+	// Equipo Enemigo
+	protected List<BattleEntity> enemyParty;
+
+	// Acciones a realizar este turno
+	protected List<Action> selectedActions;
+
+	// Entidad del jugador seleccionada en este momento
+	protected BattleEntity selectedBattleEntity;
+
+	static int SELECTING_ACTIONS = 0;
+	static int PERFORMING_ACTIONS = 1;
+
+	public BattleManager() 
+	{
+
+	}
+
+	public void update()
+	{
+
+	}
+
+	public void render()
+	{
+
+	}
+
+	public void destroy()
+	{
+
+	}
+}
