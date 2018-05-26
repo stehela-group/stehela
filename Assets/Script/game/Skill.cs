c07bc75bb32192a5eb77f7f577c9766ef368313c
Gabriel Piriz <lirianer@gmail.com>
2018-05-26 18:59:36 -0300

    Comentado TODO y cambios de estilo

--- Assets/Script/game/Skill.cs
+++ Assets/Script/game/Skill.cs
@@ -8,9 +8,17 @@
     
 
     protected int damage = 0;
-    protected int castTime = 3;
+    protected int castTime = 0;
     protected int reachTime = 0;
     protected int affectTime = 0;
+
+    /*
+        TODO: 
+            - Variables para guardar las animaciones de casting, reaching, y affecting.
+            - Sobreescribir setState para inicializar animaciones de Reaching y Affecting
+            - Funcion getDamage que devuelve el daño de la habilidad
+            - Animacion por default es la de casting que debería empezar a reproducirse cuando se crea la skill (al menos por ahora)
+     */
 
     public Skill() 
     {
@@ -23,17 +31,22 @@
         switch (this.getState())
         {
             case (Skill.CASTING):
-                //Establish the Skill Function
-                if (getTimeState > castTime)
-                    setState(REACHING);
+                if (this.getTimeState() > this.castTime)
+                {
+                    this.setState(Skill.REACHING);
+                }
                 break;
             case (Skill.REACHING):
-                if (getTimeState > reachTime) 
-                setState(AFFECTING);
+                if (this.getTimeState() > this.reachTime) 
+                {
+                    this.setState(Skill.AFFECTING);
+                }
                 break;
             case (Skill.AFFECTING):
-                if (getTimeState > affectTime)
-                    setState(FINISHED);
+                if (this.getTimeState() > this.affectTime)
+                {
+                    this.setState(Skill.FINISHED);
+                }
                 break;
             case (Skill.FINISHED):
                 break;
