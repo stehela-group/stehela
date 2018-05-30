d46681eddefcf033189df63d455159728b263d0a
Emir Nakle <emirnakle@gmail.com>
2018-05-29 22:02:23 -0300

    [WIP] Relice el Enemigo

---
+++ Assets/Script/game/entities/battle/Enemy1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+public class Enemy1 : B
+{
+     
+    public  Enemy1()
+    {
+
+        this.setName("Pedro");
+        this.Skills();
+        this.setSkillName("Hielo");
+        this.setDamage(10);
+        this.setSkillName("Atacar");
+        this.setDamage(5)
+
+
+
+
+    }
+}
