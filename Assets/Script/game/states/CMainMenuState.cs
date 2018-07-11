a8149a052004858504b03277a4da4429b17223ac
Emir Nakle <emirnakle@gmail.com>
2018-07-11 19:26:09 -0300

    Menu -Imagenes botones -Imagen Fondo

--- Assets/Script/game/states/CMainMenuState.cs
+++ Assets/Script/game/states/CMainMenuState.cs
@@ -8,6 +8,8 @@ public class CMainMenuState : CGameState
 	private CButtonSprite mButtonPlay;
     private CButtonSprite exitButton;
 
+
+
     private CText logo;
 
 	public CMainMenuState()
@@ -20,7 +22,7 @@ public class CMainMenuState : CGameState
 		base.init ();
 
 		mBackground = new CSprite ();
-		mBackground.setImage (Resources.Load<Sprite> ("Sprites/menu/menu_background"));
+		mBackground.setImage (Resources.Load<Sprite> ("Sprites/menu/menu_background2"));
 		mBackground.setXY (0, 0);
 		mBackground.setSortingLayerName("Background");
 		mBackground.setName ("background");
@@ -28,31 +30,55 @@ public class CMainMenuState : CGameState
 
 
 		mButtonPlay = new CButtonSprite ("Play");
-		mButtonPlay.setXY (CGameConstants.SCREEN_WIDTH / 2, CGameConstants.SCREEN_HEIGHT / 2);
+        mButtonPlay.setFrames(Resources.LoadAll<Sprite>("Sprites/button/play00"));
+        mButtonPlay.setName("PlayImage");
+        mButtonPlay.setXY (CGameConstants.SCREEN_WIDTH / 2, CGameConstants.SCREEN_HEIGHT / 2);
+        
+
 
         exitButton = new CButtonSprite("Exit");
+        exitButton.setFrames(Resources.LoadAll<Sprite>("Sprites/button/play00"));
+        exitButton.setName("ExitImage");
         exitButton.setXY(mButtonPlay.getX(), CGameConstants.SCREEN_HEIGHT / 3 * 2);
 
-        logo = new CText("Stehela");
+  
+
+        logo = new CText(" ");
         logo.setXY(CGameConstants.SCREEN_WIDTH / 2, CGameConstants.SCREEN_HEIGHT / 4 * 1);
-        logo.setFontSize(600f);
+        logo.setFontSize(1000f);
         logo.setPivot(0.5f, 0.5f);
         logo.setWidth(CGameConstants.SCREEN_WIDTH);
         logo.setAlignment(TMPro.TextAlignmentOptions.Center);
+         
 
-	}
+    }
 	
 	override public void update()
 	{
 		base.update ();
 
 		mButtonPlay.update ();
+
         exitButton.update();
         logo.update();
 
-		if (mButtonPlay.clicked ()) 
+        if (mButtonPlay.isMouseOver())
+
+        {
+            
+                
+            
+
+        }
+
+
+
+
+        if (mButtonPlay.clicked ()) 
 		{
 			CGame.inst ().setState(new CLevelState ());
+
+
 			return;
 		}
 
