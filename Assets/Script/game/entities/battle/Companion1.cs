c79f8abecc78dfb1b9122c362907406c535f9377
Alan Leinweber <alanleinweb@gmail.com>
2018-07-13 21:54:33 -0300

    added idle and animation.

--- Assets/Script/game/entities/battle/Companion1.cs
+++ Assets/Script/game/entities/battle/Companion1.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 public class Companion1 : BattleEntity
 {
-	public Companion1()
-	{
-		this.setName("Kairus");
+    public Companion1()
+    {
+        this.setName("Kairus");
         setFrames(Resources.LoadAll<Sprite>("Sprites/battleAlly"));
 
         this.setMaxHealth(120);
@@ -13,9 +13,47 @@ public class Companion1 : BattleEntity
         this.setScale(4);
 
         this.skills.Add(new Atacar());
-		this.skills.Add(new Curar());
+        this.skills.Add(new Curar());
+
+        setState(IDLE);
 
         this.slash.setFlip(true);
         this.slash.setXY(slash.getX() + 30, slash.getY() + 30);
+    }
+    override public void update()
+    {
+        base.update();
+
+        if (getState() == IDLE)
+        {
+
+        }
+        else if (getState() == ATTACKING)
+        {
+        }
+    }
+    override public void render()
+    {
+        base.render();
+    }
+
+    override public void destroy()
+    {
+        base.destroy();
+
+    }
+    override public void setState(int aState)
+    {
+        base.setState(aState);
+
+        if (aState == BattlePlayer.ATTACKING)
+        {
+            //this.initAnimation(10, 26, 16, false);
+        }
+
+        if (aState == BattlePlayer.IDLE)
+        {
+            this.initAnimation(1, 18, 16, true);
+        }
     }
 }
